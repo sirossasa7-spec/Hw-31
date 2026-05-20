96e5b4598e832dd226107a5a0c2d4aff84e444d1
sirossasa7@gmail.com <sirossasa7@gmail.com>
2026-05-20 23:54:45 +0200

    Car

--- Hw-31/Car.cpp
+++ Hw-31/Car.cpp
@@ -1,1 +1,26 @@
 #include "Car.h"
+#include <iostream>
+
+using namespace std;
+
+Car::Car(double p, double s, int y, int d)
+{
+    price = p;
+    maxSpeed = s;
+    year = y;
+    doors = d;
+}
+
+string Car::getType() const
+{
+    return "Car";
+}
+
+void Car::show() const
+{
+    cout << "Car\n";
+    cout << "Price: " << price << endl;
+    cout << "Speed: " << maxSpeed << endl;
+    cout << "Year: " << year << endl;
+    cout << "Doors: " << doors << endl;
+}
--- Hw-31/Car.h
+++ Hw-31/Car.h
@@ -1,7 +1,14 @@
 #pragma once
 #include "Vehicle.h"
-class Car :
-    public Vehicle
-{
-};
 
+class Car : public Vehicle
+{
+private:
+    int doors;
+
+public:
+    Car(double price, double speed, int year, int doors);
+
+    string getType() const override;
+    void show() const override;
+};
