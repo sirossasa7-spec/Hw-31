d92f05686df1c97689f5f2cc4c229d043a534717
sirossasa7@gmail.com <sirossasa7@gmail.com>
2026-05-20 23:52:01 +0200

    Vehicle.cpp

--- Hw-31/Vehicle.cpp
+++ Hw-31/Vehicle.cpp
@@ -1,16 +1,45 @@
 #include "Vehicle.h"
 
-Vehicle::Vehicle() : price(0), maxSpeed(0), year(0) {}
-
-Vehicle::Vehicle(double price, double maxSpeed, int year)
-    : price(price), maxSpeed(maxSpeed), year(year) {
+Vehicle::Vehicle()
+{
+    price = 0;
+    maxSpeed = 0;
+    year = 0;
 }
 
-void Vehicle::setPrice(double price) { this->price = price; }
-double Vehicle::getPrice() const { return price; }
+Vehicle::Vehicle(double p, double s, int y)
+{
+    price = p;
+    maxSpeed = s;
+    year = y;
+}
 
-void Vehicle::setMaxSpeed(double speed) { this->maxSpeed = speed; }
-double Vehicle::getMaxSpeed() const { return maxSpeed; }
+void Vehicle::setPrice(double p)
+{
+    price = p;
+}
 
-void Vehicle::setYear(int year) { this->year = year; }
-int Vehicle::getYear() const { return year; }
+double Vehicle::getPrice() const
+{
+    return price;
+}
+
+void Vehicle::setMaxSpeed(double s)
+{
+    maxSpeed = s;
+}
+
+double Vehicle::getMaxSpeed() const
+{
+    return maxSpeed;
+}
+
+void Vehicle::setYear(int y)
+{
+    year = y;
+}
+
+int Vehicle::getYear() const
+{
+    return year;
+}
